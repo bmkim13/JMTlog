9098f773cdb16dc637f287ca539deeb50928b6d5
js.lee <meteorjin@gmail.com>
2020-02-10 15:50:46 +0900

    Update background.js

--- JMTTOOLS/jmtChromeExt/background.js
+++ JMTTOOLS/jmtChromeExt/background.js
@@ -1,8 +1,5 @@
 console = chrome.extension.getBackgroundPage().console;
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-});
-
 var gSelectedInfo = {
     url: "",
     selectedText: "",
@@ -25,13 +22,70 @@ var clickHandler = function (e) {
 chrome.contextMenus.create({
     "title" : "JMT",
     "contexts" : ["selection", "image"],
-    "onclick" : clickHandler
+    "onclick" : clickHandler,
 });
 
 // Listen for messages from the popup.
 chrome.runtime.onMessage.addListener(function (msg, sender, response) {
-    console.log("onMessage : " + msg);
-    if ((msg.from === 'popup') && (msg.subject === 'selectedInfo')) {
+    console.log("onMessage : " + msg.from);
+    if ((msg.from === 'window') && (msg.subject === 'selectedInfo')) {
       response(gSelectedInfo);
     }
+    else if ((msg.from === 'content') && (msg.subject === 'selectedText')) {
+      gSelectedInfo.selectedText = msg.data
+      gSelectedInfo.url = msg.url
+    } 
+    else if ((msg.from === 'content') && (msg.subject === 'imageUrl')) {
+      gSelectedInfo.imageUrl = msg.data
+      gSelectedInfo.url = msg.url
+    } 
 });
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    showWindows(function (result) {
+    })
+});
+
+var bShowWindow = false
+var windowId = -1
+function showWindows (callback) {
+    if (bShowWindow) {
+        bShowWindow = false
+        chrome.windows.get(windowId, function(win) {
+            if (win === undefined) {
+                createWindow(callback)
+            } else {
+                chrome.windows.remove(windowId)
+                chrome.windows.onRemoved.removeListener(windowRemovedListener);
+                callback(bShowWindow)
+            }
+        });
+    } else {
+        return createWindow(callback)
+    }
+}
+
+function createWindow(callback) {
+    var windowWidth = 450;
+    var windowHeight = 325;
+    chrome.windows.create(
+        {
+            url: chrome.runtime.getURL("window.html"),
+            type: "popup",
+            width : windowWidth,
+            height : windowHeight
+        }, 
+        function(win) {
+            windowId = win.id
+            bShowWindow = true
+            chrome.windows.onRemoved.addListener(windowRemovedListener);
+            callback(bShowWindow)
+        }
+    );
+}
+
+function windowRemovedListener(winId) {
+    if (winId == windowId) {
+        bShowWindow = false
+    }
+}
