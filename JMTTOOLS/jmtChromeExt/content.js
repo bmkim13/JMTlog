eb15940b65e28deb23934219f8b72df4f0ee3804
js.lee <meteorjin@gmail.com>
2020-02-11 15:34:28 +0900

    Update content.js

--- JMTTOOLS/jmtChromeExt/content.js
+++ JMTTOOLS/jmtChromeExt/content.js
@@ -1,9 +1,11 @@
+'use strict';
 
 document.addEventListener('mouseup',function(event) {
-    if (event.shiftKey) {
+    if (event.ctrlKey && event.shiftKey) {
         var sel = window.getSelection().toString();
         if(sel.length) {
-            highlightText(event.target);
+            removeAllElements();
+            selectedTexts = "";
             chrome.runtime.sendMessage(
                 {
                     from: "content",
@@ -12,22 +14,12 @@ document.addEventListener('mouseup',function(event) {
                     data: sel
                 });
         }
-        if (event.altKey) {
-            chrome.runtime.sendMessage(
-                {
-                    from: "content",
-                    subject: "selectedText",
-                    url: event.target.href,
-                    data: ""
-                });
-        }
     }
 })
 
-var selectedText = null
-
+var selectedTexts = ""
 document.addEventListener('mousedown',function(event) {
-    if (event.shiftKey) {
+    if (event.ctrlKey && !event.altKey) {
         if (event.target.nodeName == "IMG") {
             highlightImg(event.srcElement);
             chrome.runtime.sendMessage(
@@ -37,48 +29,79 @@ document.addEventListener('mousedown',function(event) {
                     url: event.target.href,
                     data: event.target.src
                 });
-        }
-        if (event.ctrlKey) {
-            if (event.target.nodeName == "P") {
-                highlightText(event.target);
-                if (selectedText == null) {
-                    selectedText = event.target.innerText
-                } else {
-                    selectedText += event.target.innerText
-                }
-                chrome.runtime.sendMessage(
-                    {
-                        from: "content",
-                        subject: "selectedText",
-                        url: event.target.href,
-                        data: selectedText
-                    });
+        } else if (isTextTag(event.target.nodeName)) {
+            if (isAdded(event.target)) {
+                removeElement(event.target);
+                selectedTexts = makeTextFromSelected();
+            } else {
+                addElement(event.target);
+                selectedTexts += event.target.innerText
             }
-        } else if (event.target.nodeName == "P") {
-            highlightText(event.target);
-            selectedText = event.target.innerText
             chrome.runtime.sendMessage(
                 {
                     from: "content",
                     subject: "selectedText",
                     url: event.target.href,
-                    data: selectedText
+                    data: selectedTexts
                 });
         }
+    } else if (event.ctrlKey && event.altKey) {
+        selectedTexts = ""
+        removeAllElements();
+        chrome.runtime.sendMessage(
+            {
+                from: "content",
+                subject: "selectedText",
+                url: event.target.href,
+                data: ""
+            });
     }
 })
 
-var prevText = null;
-var MOUSE_SELECTED_TEXT_CLASSNAME = 'mouse_selected_text';
-function highlightText(srcElement) {
-    if (prevText != null) {
-        prevText.classList.remove(MOUSE_SELECTED_TEXT_CLASSNAME);
+function makeTextFromSelected() {
+    var result = "";
+    for (var i = 0; i < selectedElements.length; i++) {
+        result += selectedElements[i].innerText
     }
-    srcElement.classList.add(MOUSE_SELECTED_TEXT_CLASSNAME);
-    prevText = srcElement;
-    
+    return result;
 }
 
+function isTextTag(nodeName){
+    if (nodeName == "P")
+        return true;
+    if (nodeName == "BLOCKQUOTE")
+        return true;
+
+    if (nodeName.innerText !== undefined && !(nodeName.innerText.length === 0 || !nodeName.innerText.trim()))
+        return true;
+        
+    return false;
+}
+
+var selectedElements = [];
+function isAdded(srcElement) {
+     return selectedElements.includes(srcElement)
+}
+
+var MOUSE_SELECTED_TEXT_CLASSNAME = 'mouse_selected_text';
+function addElement(srcElement) {
+    srcElement.classList.add(MOUSE_SELECTED_TEXT_CLASSNAME);
+    selectedElements.push(srcElement);
+}
+
+function removeElement(srcElement) {
+    srcElement.classList.remove(MOUSE_SELECTED_TEXT_CLASSNAME);
+    selectedElements = selectedElements.filter(function( obj ) {
+        return obj !== srcElement;
+    });
+}
+
+function removeAllElements() {
+    for (var i = 0; i < selectedElements.length; i++) {
+        selectedElements[i].classList.remove(MOUSE_SELECTED_TEXT_CLASSNAME);
+    }
+    selectedElements = [];
+}
 
 var prevImg = null;
 var MOUSE_SELECTED_IMG_CLASSNAME = 'mouse_selected_image';
